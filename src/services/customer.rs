08e3ed2d7478134188979d0c8627aed34047245c
pongwisanu <pongwisanu.hut@nida.ac.th>
2024-08-15 15:40:31 +0700

    update service

--- src/services/customer.rs
+++ src/services/customer.rs
@@ -2,24 +2,24 @@ use std::convert::Infallible;
 use warp::{self, Filter};
 
 use crate::db::Db;
-use crate::handlers;
+use crate::handlers::customer;
 use crate::models::customer::Customer;
 
 pub fn customer_routes(
     db: Db,
 ) -> impl Filter<Extract = impl warp::Reply, Error = warp::Rejection> + Clone {
-        list_customer(db.clone())
+        get_customer(db.clone())
             .or(create_customer(db.clone()))
-            .or(get_customer(db.clone()))
             .or(update_customer(db.clone()))
             .or(delete_customer(db.clone()))
+            .or(list_customer(db.clone()))
 }
 
 fn list_customer(db: Db) -> impl Filter<Extract = impl warp::Reply, Error = warp::Rejection> + Clone {
     warp::path("customers")
         .and(warp::get())
         .and(with_db(db))
-        .and_then(handlers::customer::list_mails)
+        .and_then(customer::list_mails)
 }
 
 fn create_customer(db: Db) -> impl Filter<Extract = impl warp::Reply, Error = warp::Rejection> + Clone {
@@ -27,14 +27,14 @@ fn create_customer(db: Db) -> impl Filter<Extract = impl warp::Reply, Error = wa
         .and(warp::post())
         .and(json_body())
         .and(with_db(db))
-        .and_then(handlers::customer::create_customer)
+        .and_then(customer::create_customer)
 }
 
 fn get_customer(db: Db) -> impl Filter<Extract = impl warp::Reply, Error = warp::Rejection> + Clone {
     warp::path!("customers" / String)
         .and(warp::get())
         .and(with_db(db))
-        .and_then(handlers::customer::get_customer)
+        .and_then(customer::get_customer)
 }
 
 fn update_customer(db: Db) -> impl Filter<Extract = impl warp::Reply, Error = warp::Rejection> + Clone {
@@ -42,14 +42,14 @@ fn update_customer(db: Db) -> impl Filter<Extract = impl warp::Reply, Error = wa
         .and(warp::put())
         .and(json_body())
         .and(with_db(db))
-        .and_then(handlers::customer::update_customer)
+        .and_then(customer::update_customer)
 }
 
 fn delete_customer(db: Db) -> impl Filter<Extract = impl warp::Reply, Error = warp::Rejection> + Clone {
     warp::path!("customers" / String)
         .and(warp::delete())
         .and(with_db(db))
-        .and_then(handlers::customer::delete_customer)
+        .and_then(customer::delete_customer)
 }
 
 fn with_db(db: Db) -> impl Filter<Extract = (Db,), Error = Infallible> + Clone {
